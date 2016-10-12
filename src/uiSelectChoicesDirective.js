2863421cc4968af7cddfa3ab10aab567b90d2445
eggbeard <michael.hodgson@gmail.com>
2016-10-12 10:31:30 -0400

    fix: Check for existance of add-groups attribute to add event to group header

--- src/uiSelectChoicesDirective.js
+++ src/uiSelectChoicesDirective.js
@@ -29,7 +29,7 @@ uis.directive('uiSelectChoices',
         var groups = tElement.querySelectorAll('.ui-select-choices-group');
         if (groups.length !== 1) throw uiSelectMinErr('rows', "Expected 1 .ui-select-choices-group but got '{0}'.", groups.length);
         //Add click handler to the group
-        if(addGroups){
+        if(typeof addGroups !== 'undefined'){
           var groupLabels = tElement.querySelectorAll('.ui-select-choices-group-label');
           groupLabels.attr('ng-click', '$select.headerClick($group,$select.skipFocusser,$event)');
         }
